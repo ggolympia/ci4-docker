81d4447ff12a41d9f987a7acbffffe919c1011a0
George Garzon Mac <george.garzon@olympiapharmacy.com>
2025-01-14 23:21:46 -0500

    database update? 2

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -24,43 +24,16 @@ class Database extends Config
      *
      * @var array<string, mixed>
      */
-//    public array $default = [
-//        'DSN'          => '',
-//        'hostname'     => 'db',
-//        'username'     => 'root',
-//        'password'     => '',
-//        'database'     => 'ci4',
-//        'DBDriver'     => 'MySQLi',
-//        'DBPrefix'     => '',
-//        'pConnect'     => false,
-//        'DBDebug'      => true,
-//        'charset'      => 'utf8mb4',
-//        'DBCollat'     => 'utf8mb4_general_ci',
-//        'swapPre'      => '',
-//        'encrypt'      => false,
-//        'compress'     => false,
-//        'strictOn'     => false,
-//        'failover'     => [],
-//        'port'         => 3307,
-//        'numberNative' => false,
-//        'dateFormat'   => [
-//            'date'     => 'Y-m-d',
-//            'datetime' => 'Y-m-d H:i:s',
-//            'time'     => 'H:i:s',
-//        ],
-//    ];
-
     public array $default = [
         'DSN'          => '',
-        'hostname'     => '5.161.220.216',
-        'username'     => 'mysql',
-        'password'     => 'MHv1JKcqbgrB0bWGJa1KLqa4Ja4KhGZl1XUlwwYvqjHnjnBLhNURpxQAMDNhD3H6',
-        'database'     => 'default',
-//        'DBDriver'     => 'MySQLi',
+        'hostname'     => 'db',
+        'username'     => 'root',
+        'password'     => '',
+        'database'     => 'ci4',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
-        'DBDebug'      => true, // Set to true here
+        'DBDebug'      => true,
         'charset'      => 'utf8mb4',
         'DBCollat'     => 'utf8mb4_general_ci',
         'swapPre'      => '',
@@ -68,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 5432, // Ensure this matches your MySQL port (default 3306)
+        'port'         => 3307,
         'numberNative' => false,
         'dateFormat'   => [
             'date'     => 'Y-m-d',
@@ -76,6 +49,33 @@ class Database extends Config
             'time'     => 'H:i:s',
         ],
     ];
+
+//    public array $default = [
+//        'DSN'          => '',
+//        'hostname'     => '5.161.220.216',
+//        'username'     => 'mysql',
+//        'password'     => 'MHv1JKcqbgrB0bWGJa1KLqa4Ja4KhGZl1XUlwwYvqjHnjnBLhNURpxQAMDNhD3H6',
+//        'database'     => 'default',
+////        'DBDriver'     => 'MySQLi',
+//        'DBDriver'     => 'MySQLi',
+//        'DBPrefix'     => '',
+//        'pConnect'     => false,
+//        'DBDebug'      => true, // Set to true here
+//        'charset'      => 'utf8mb4',
+//        'DBCollat'     => 'utf8mb4_general_ci',
+//        'swapPre'      => '',
+//        'encrypt'      => false,
+//        'compress'     => false,
+//        'strictOn'     => false,
+//        'failover'     => [],
+//        'port'         => 5432, // Ensure this matches your MySQL port (default 3306)
+//        'numberNative' => false,
+//        'dateFormat'   => [
+//            'date'     => 'Y-m-d',
+//            'datetime' => 'Y-m-d H:i:s',
+//            'time'     => 'H:i:s',
+//        ],
+//    ];
 
     //    /**
     //     * Sample database connection for SQLite3.
